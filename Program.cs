17195174fead64aec14d5102e3cd14733a62ff18
Hampus Sandström <sandstroms.hampus@gmail.com>
2024-11-14 13:33:32 +0100

    Fixat

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace Query_Expressions_Gruppövning
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
     using System.Linq;
+    using System.Threading.Tasks.Dataflow;
 
     class Product
     {
@@ -27,12 +29,44 @@
 
         static void Main(string[] args)
         {
+            int amount = 0;
             InventoryFileGenerator inventoryFileGenerator = new InventoryFileGenerator();
             inventoryFileGenerator.GenerateInventoryFile("inventory.txt", 5000);
             var prod = LoadInventoryData();
 
             // Implementera query expressions här
+            //Lista alla produkter i kategorin "Verktyg" sorterade efter pris(stigande).
+            var products = from p in inventory
+                           where p.Category == "Verktyg"
+                           orderby p.Price ascending
+                           select p;
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Produkt: {product.Name} kostar {product.Price}Kr");
+            }
+            //Gruppera produkterna efter kategori och visa antalet produkter i varje kategori. 
+            var grouping = from p in inventory
+                           group p by p.Category into katGroup
 
+                           orderby katGroup.Key
+
+                           select new
+                           {
+                               katName = katGroup.Key,
+                               produkter = katGroup.ToList(),
+                               TotalQuantity = katGroup.Sum(x => x.Quantity)
+                           };
+            foreach (var catName in grouping)
+            {
+                Console.WriteLine(catName.katName);
+                Console.WriteLine($"Antal produkter: {catName.TotalQuantity}");
+            }
+            //Hitta den kategori som har det högsta genomsnittliga priset per produkt. 
+            var priceMax = (from p in inventory
+                            group p by p.Category into priceCat
+                            select priceCat.Average(x => x.Price)).Max();
+                           
+            Console.WriteLine($"{priceMax:C2}");
             Console.ReadLine();
         }
 
